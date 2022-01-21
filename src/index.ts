1579db2ba8adf95c1c0a7757c9af6432e097d919
Darren McElligott <darren.mcelligott@gmail.com>
2022-01-21 15:36:55 -0500

    use a new search algo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import * as puppeteer from 'puppeteer';
 import { Page } from 'puppeteer';
 
-// import * as words from './words.json';
+import * as words from './wordle-all-words.json';
+import { GuessState, Letter, Position, WordleTrie } from './WordleTrie';
 
-import * as words from './wordle-words.json';
-
-const MOST_COMMONLY_USED_LETTERS = ['a', 'e', 's', 'o', 'r', 'i', 'l', 't'];
 const MOST_COMMONLY_USED_WORDS = [
   // 'aries',
   // 'orias',
@@ -38,6 +36,7 @@ type ValidLetter = {
   state: TileState;
 }
 
+const trie = new WordleTrie(words);
 
 const clearRow = async (page: Page) => {
   await page.keyboard.press('Backspace');
@@ -67,69 +66,9 @@ const getShareButton = async (page: Page) => {
   return await page.evaluateHandle(`document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-modal > game-stats").shadowRoot.querySelector("#share-button")`);
 };
 
-const getWord = (validLetters: ValidLetter[], invalidLetters: string[], guessedWords: string[], tryIndex: number = 0) => {
-  const map = new Map();
-  const filteredValidLetters = [];
-
-  console.log(invalidLetters);
-  console.log(validLetters);
-  console.log(filteredValidLetters);
-
-  validLetters.forEach(letter => {
-
-  });
-
-  const filteredWords = words
-    .filter(word => word.split('').every(letter => !invalidLetters.includes(letter)))
-    .filter(word => !guessedWords.includes(word));
-
-  console.log('wordlist length', filteredWords.length);
-
-  filteredWords.find(word => {
-    let correct = 0;
-    let present = 0;
-
-    validLetters.forEach(({ value, position, state }) => {
-      if (state === TileState.Correct) {
-        if (word.includes(value, position)) {
-          correct += 1;
-        }
-      }
-
-      if (state === TileState.Present) {
-        if (word.includes(value)) {
-          present += 1;
-        }
-      }
-    });
-
-    if (correct > 0 || present > 0) {
-      map.set(word, {
-        correct,
-        present
-      });
-    }
-  });
-
-  const ordered = Array.from(map.entries()).sort((a, b) => {
-    if (b[1].correct > 0) {
-      return b[1].correct - a[1].correct;
-    }
-
-    if (b[1].present > 0) {
-      return b[1].present - a[1].present;
-    }
-  });
-
-  console.log(ordered);
-
-  return ordered[tryIndex][0];
-}
-
-const makeGuess = async (page: Page, word: string, hasGuessedMostCommon = false, validLetters: Array<ValidLetter>, invalidLetters: string[], maxTries: number = 0, tryIndex: number = 0, guess: number = 0, guessedWords = []) => {
+const makeGuess = async (page: Page, word: string, hasGuessedMostCommon = false, guessState: GuessState, maxTries: number = 0, tryIndex: number = 0, guess: number = 0, guessedWords = []) => {
   await page.type('body', word);
   await page.keyboard.press('Enter');
-  const notInAcceptableWordlist = await getNotInListToast(page);
   const fail = await getFailToast(page);
   const success = await getSuccessToast(page);
 
@@ -150,57 +89,42 @@ const makeGuess = async (page: Page, word: string, hasGuessedMostCommon = false,
 
   console.log('Trying "', word, '"');
 
-  if (notInAcceptableWordlist) {
-    await clearRow(page);
-
-    if (maxTries > 10) { // FIXME remove this when we're done
-      console.log('Exceeded maxTries:', maxTries);
-      return; // Bailing early to test logic
-    }
-
-    console.log('word "', word, '" was not in the word list');
-    if (!hasGuessedMostCommon) {
-      const mostCommonlyUsedWordIndex = MOST_COMMONLY_USED_WORDS.findIndex(value => value === word);
-
-      if (mostCommonlyUsedWordIndex === MOST_COMMONLY_USED_WORDS.length - 1) {
-        console.log('Exhausted all the most commonly used words');
-        const nextWord = getWord(validLetters, invalidLetters, guessedWords, tryIndex);
-        console.log('Trying next word', nextWord);
-        return makeGuess(page, nextWord, true, validLetters, invalidLetters, maxTries + 1, tryIndex + 1, guess, [...guessedWords, word]);
-      }
-
-      return makeGuess(page, MOST_COMMONLY_USED_WORDS[mostCommonlyUsedWordIndex + 1], hasGuessedMostCommon, validLetters, invalidLetters,maxTries + 1, tryIndex + 1, guess, [...guessedWords, word]);
-    } else {
-      console.log('trying next');
-      const nextWord = getWord(validLetters, invalidLetters, guessedWords, tryIndex + 1);
-      return makeGuess(page, nextWord, true, validLetters, invalidLetters, maxTries + 1, tryIndex + 1, guess, [...guessedWords, word]);
-    }
-  }
-
   const row = await getRow(page, guess);
 
+  const newGuessState = { ...guessState };
+
   Object.values(row.$tiles).forEach((tile: Tile, index: number) => {
+    const letter = tile._letter as Letter;
+    const position = index as Position;
     switch (tile._state) {
       case TileState.Absent:
-        // Yo, let's pass `invalidLetters`, too - we can just ignore any words without these
-        invalidLetters = [...invalidLetters, tile._letter];
+        !newGuessState.absent.includes(letter) && newGuessState.absent.push(letter);
         break;
       case TileState.Present:
-        validLetters.push({ value: tile._letter, position: index, state: TileState.Present });
+        const existing = newGuessState.present.find(p => p.letter === letter);
+        if (existing) {
+          !existing.excludedPosition.includes(position) && existing.excludedPosition.push(position);
+        } else {
+          newGuessState.present.push({ letter, excludedPosition: [position] });
+        }
         break;
       case TileState.Correct:
-        validLetters.push({ value: tile._letter, position: index, state: TileState.Correct });
+        if (!newGuessState.correct.some((c) => c.letter === letter && c.position === position)) {
+          newGuessState.correct.push({ letter, position });
+        }
         break;
     }
   });
 
   console.log('trying next');
-  const nextWord = getWord(validLetters, invalidLetters, [...guessedWords, word], 0);
-  return makeGuess(page, nextWord, true, validLetters, invalidLetters, maxTries + 1, 0, guess + 1, [...guessedWords, word]);
+  const nextWord = trie.search(guessState);
+  if (!nextWord) {
+    throw new Error('no words left');
+  }
+  return makeGuess(page, nextWord, true, newGuessState, maxTries + 1, 0, guess + 1, [...guessedWords, word]);
 };
 
 (async() => {
-
   const browser = await puppeteer.launch({
     headless: false,
     // slowMo: 500,
@@ -209,6 +133,8 @@ const makeGuess = async (page: Page, word: string, hasGuessedMostCommon = false,
 
   const page = await browser.newPage();
 
+  // await page.emulateTimezone('Asia/Singapore');
+
   await page.goto('https://www.powerlanguage.co.uk/wordle');
 
   await page.waitForTimeout(1000);
@@ -216,6 +142,6 @@ const makeGuess = async (page: Page, word: string, hasGuessedMostCommon = false,
   const instructionsCloseButton = await page.evaluateHandle(`document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-modal").shadowRoot.querySelector("div > div > div")`);
   await instructionsCloseButton.asElement().click();
 
-  await makeGuess(page, MOST_COMMONLY_USED_WORDS[0], false, [], [], 0, 0, 0);
+  await makeGuess(page, MOST_COMMONLY_USED_WORDS[Math.floor(Math.random() * (MOST_COMMONLY_USED_WORDS.length - 1))], false, WordleTrie.initialGuessState(), 0, 0, 0);
   await browser.close();
 })();
